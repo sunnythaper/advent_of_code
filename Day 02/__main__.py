82e83e454bbadfbd1ff398f1548fefa9eede5620
Sunny Thaper <sthaper@gmail.com>
2023-12-02 00:47:01 -0700

    Day 2 Puzzle 2 Answer

--- Day 02/__main__.py
+++ Day 02/__main__.py
@@ -10,7 +10,11 @@ class Round(BaseModel):
 
 class Game(BaseModel):
     id: int
+    power: int | None = None
     possible: bool | None = None
+    min_red_cubes: int | None = None
+    min_green_cubes: int | None = None
+    min_blue_cubes: int | None = None
     rounds: list[Round] | None = None
 
 class Day2:
@@ -21,10 +25,11 @@ class Day2:
       self.file = "list.txt"
       self.games = []
       self.sum = 0
+      self.power = 0
       self.process_games()
-      self.sum_possible()
       print(self.games)
       print(self.sum)
+      print(self.power)
 
     def process_games(self) -> None:
       with open(self.file, "r") as file:
@@ -33,6 +38,10 @@ class Day2:
           rounds = self.process_rounds(line)
           self.games.append(Game(id=id, rounds=rounds))
       self.check_possible()
+      self.sum_possible()
+      self.minimum_needed_cubes()
+      self.set_power()
+      self.sum_power()
 
     def process_rounds(self, line: str) -> list[Round]:
       rounds = []
@@ -68,6 +77,30 @@ class Day2:
       for game in self.games:
         if game.possible:
           self.sum += game.id
+
+    def minimum_needed_cubes(self) -> None:
+      for game in self.games:
+        max_red_cubes = 0
+        max_green_cubes = 0
+        max_blue_cubes = 0
+        for round in game.rounds:
+          if round.red_cubes > max_red_cubes:
+            max_red_cubes = round.red_cubes
+          if round.green_cubes > max_green_cubes:
+            max_green_cubes = round.green_cubes
+          if round.blue_cubes > max_blue_cubes:
+            max_blue_cubes = round.blue_cubes
+        game.min_red_cubes = max_red_cubes
+        game.min_green_cubes = max_green_cubes
+        game.min_blue_cubes = max_blue_cubes
+
+    def set_power(self) -> None:
+      for game in self.games:
+        game.power = game.min_red_cubes * game.min_green_cubes * game.min_blue_cubes
+
+    def sum_power(self) -> None:
+      for game in self.games:
+        self.power += game.power
 
 if __name__ == "__main__":
     Day2()
