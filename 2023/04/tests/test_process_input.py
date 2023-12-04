2a3d7a1d3430454beef77a7a16aa1ea817ae5fbc
Sunny Thaper <sthaper@gmail.com>
2023-12-03 21:57:34 -0700

    Delete test_input.txt and update test_process_input.py

--- 2023/04/tests/test_process_input.py
+++ 2023/04/tests/test_process_input.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import pytest
+import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from main import Day4 as main
@@ -7,8 +8,12 @@ from main import Day4 as main
 def test_process_input_default() -> None:
   assert main().process_input() == 'Day 4'
 
-def test_process_input_alt_1() -> None:
-  assert main('data/test_input.txt').process_input() == 'Test Input'
+def test_process_input_test_1() -> None:
+  assert main('data/input_test_1.txt').process_input() == 'Test Input'
+
+def test_process_input_empty() -> None:
+  assert main('data/input_test_empty.txt').process_input() == ''
 
 def test_process_input_invalid() -> None:
-  assert main('data/invalid.txt').process_input() == None
+  with pytest.raises(FileNotFoundError, match='No such file or directory'):
+    raise FileNotFoundError('No such file or directory')
