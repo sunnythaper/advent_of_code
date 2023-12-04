43901f70b2bcfc7a3331cab7755cbc0b0503e421
Sunny Thaper <sthaper@gmail.com>
2023-12-03 20:41:55 -0700

    Refactor test_process_input function names

--- 2023/04/tests/test_main.py
+++ 2023/04/tests/test_main.py
@@ -4,8 +4,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from main import Day4 as main
 
-def test_process_input() -> None:
+def test_process_input_default() -> None:
   assert main().process_input() == 'Day 4'
 
-def test_process_input_alt() -> None:
+def test_process_input_alt_1() -> None:
   assert main('data/test_input.txt').process_input() == 'Test Input'
